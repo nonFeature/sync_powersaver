240428a7b827e6d0483997a8a96a0a393e4d2f67
tecxz5 <zamulatimofei@yandex.ru>
2026-08-16 20:42:09 +0300

    лан

--- i18n/locales.py
+++ i18n/locales.py
@@ -2,12 +2,12 @@ from java.util import Locale
 
 STRINGS = {
     "en": {
-        "plugin_obsolete": 'The features of this plugin already work here, so the plugin will delete itself.\n\nTap "OK" to go to the power saving settings.',  # noqa: E501
+        "plugin_obsolete": 'The features of this plugin already work here, so the plugin will delete itself.\n\nTap "OK" to go to the power saving settings.',
         "btn_close": "Close",
         "btn_ok": "OK",
     },
     "ru": {
-        "plugin_obsolete": 'Функции данного плагина уже здесь работают, так что плагин удалится сам.\n\nНажми «ОК», чтобы перейти в настройки энергосбережения.',  # noqa: E501
+        "plugin_obsolete": "Функции данного плагина уже здесь работают, так что плагин удалится сам.\n\nНажми «ОК», чтобы перейти в настройки энергосбережения.",  # noqa: E501
         "btn_close": "Закрыть",
         "btn_ok": "ОК",
     },
