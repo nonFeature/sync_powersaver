9c04f67a2ad3f959fa0fcc637f3a3a826da331f3
tecxz5 <zamulatimofei@yandex.ru>
2026-08-16 20:41:00 +0300

    менее официальная локаль

--- i18n/locales.py
+++ i18n/locales.py
@@ -2,12 +2,12 @@ from java.util import Locale
 
 STRINGS = {
     "en": {
-        "plugin_obsolete": 'The functionality of this plugin is already implemented in this app version.\n\nPressing "OK" will redirect you to the power saving settings.',  # noqa: E501
+        "plugin_obsolete": 'The features of this plugin already work here, so the plugin will delete itself.\n\nTap "OK" to go to the power saving settings.',  # noqa: E501
         "btn_close": "Close",
         "btn_ok": "OK",
     },
     "ru": {
-        "plugin_obsolete": 'Функционал этого плагина уже реализован в этой версии приложения.\n\nПри нажатии кнопки "OK", вас перенаправит в настройки энергосбережения.',  # noqa: E501
+        "plugin_obsolete": 'Функции данного плагина уже здесь работают, так что плагин удалится сам.\n\nНажми «ОК», чтобы перейти в настройки энергосбережения.',  # noqa: E501
         "btn_close": "Закрыть",
         "btn_ok": "ОК",
     },
